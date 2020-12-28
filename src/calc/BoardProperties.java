f79b732791d095243e26ed79bb0edd3c4264a64e
Tim Pöschl <64636296+timmitohnetim@users.noreply.github.com>
2020-12-28 14:35:53 +0100

    Kommentare in BoardProperties

--- src/calc/BoardProperties.java
+++ src/calc/BoardProperties.java
@@ -1,9 +1,11 @@
 package calc;
 
+// dient zum Übergeben der Startpositionen an AlgorithmThread
+
 public class BoardProperties {
 	
-	public int[] boardIntegers;
-	public int symmetry;
+	public int[] boardIntegers;			// N Integers, deren Bit-Darstellung jeweils der Belegung der Zeile entspricht
+	public int symmetry;				// Lösung wird je nach Symmetrie 2x od. 4x od. 8x gezählt (symmetry ist 2 od. 4 od. 8)
 	
 	public BoardProperties(int[] boardIntegers, int symmetry) {
 		this.boardIntegers = boardIntegers;
